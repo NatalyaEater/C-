87792cf4beb40afbc1472ea76717f7e0e3e31a64
Natasha Eater <natalya.iter@yandex.ru>
2022-10-14 00:29:57 +0300

    ignore

--- Desktop/lp/stud/cisharp/1seminar/zadacha4/zadacha4dz.cs
+++ Desktop/lp/stud/cisharp/1seminar/zadacha4/zadacha4dz.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int numberE = int.Parse(Console.ReadLine());
+int max = numberA;
+if (numberA>max)
+{
+    max = numberA;
+}
+if(numberB>max)
+{
+    max = numberB;
+}
+if(numberE>max)
+{
+    max = numberE;
+}
+ Console.WriteLine(max);
